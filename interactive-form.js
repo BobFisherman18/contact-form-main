bc61e8e620a83d10d51c43eae00781c0ef5b17c4
BobFisherman18 <jonahrivera2010@hotmail.com>
2024-07-24 16:16:38 -0700

    Finished contact form

---
+++ interactive-form.js
@@ -0,0 +1,12 @@
+const enquiryField = document.getElementById('gen-enquiry-field');
+const enquiry = document.getElementById('gen-enquiry');
+const requestField = document.getElementById('s-request-field');
+const request = document.getElementById('s-request');
+
+
+function queryClicked() {
+
+
+enquiryField.style.backgroundColor = "hsl(148, 38%, 91%)";
+requestField.style.backgroundColor = "hsl(148, 38%, 91%)";
+}
